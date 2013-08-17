fd2252cd2ca1f3f57f3cb0ba92d953e4201539b9
Mashashi Nakamura <mashashi.nakamura@gmail.com>
2013-08-17 22:30:39 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Description
+
+RingSMS allows you to send messages for free. Even if you are disconnected from the internet. It does so by calling the number to you wish to send a message in a pattern that can be recognized as usefull information (your written tesxt). It uses a advanced algorithm to identify unequivocally which character is being transmitted by calling the targeted number of the message the minimum possible times. It also employs a feature of error detection to make sure most of the times, the message you want to send is what reaches the other end. This feature can be turn on or off as it implies the transmission of extra information.
+In order for the message to be correctly interpreted the user which you contact must have this same program installed on his or her phone. And the end user most not answer the call.
+
+The logical question would be... "How do I know when I receive a regular call and a message?". RingSMS is solves this problem with the following approach at the first tone that it receives from the call that is incoming, if it takes to long, the application assumes that it can not be a message and displays a notification saying that is alright to pick up the phone call.
+
+You can also share a list of strings with friends which have predefined identification making it possible to send useful information in a few time. This feature is called code map using. Within the application.
+
+The LICENSE.MD file is only valid to the source code available on this page. The application RingSMS it self is copyright for now.
+
+You if are looking for the packaged version of the tools used to integrate 3rd party software with RingSMS Pro consider looking for it in:
+https://github.com/Mashashi/ringsms/releases
---
+++ RingSMSDevTools_v1.0/src/pt/mashashi/ringsms/RingSMS.java
@@ -0,0 +1,124 @@
+package pt.mashashi.ringsms;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
+public class RingSMS {
+	
+	private RingSMS(){
+		throw new IllegalArgumentException("This isn't instantiable.");
+	}
+	
+	/*public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException, RequestOverAllowedMaxCharactersException {
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(2013, 7, 14, 16, 3, 0);
+		System.out.println(buildRequestCode("aaaa", 555555555, "916557758", cal.getTime(), "00000000.00000000"));
+		
+		String password = transformIn256BitHexKey("oi");
+		System.out.println(password);
+		System.out.println(password.length());
+		
+	}*/
+	
+	/*public static void main(String[] args) {
+		System.out.println("Nothing to see here");
+	}*/
+	
+	public static final int MAXIMUM_CHARACTERS_ALLOWED = 80;
+	
+	@SuppressWarnings("serial")
+	public static class RequestOverAllowedMaxCharactersException extends Exception{
+		public RequestOverAllowedMaxCharactersException() {
+			super("The resulting code would have more then 80 characters.");
+		}
+	}
+	
+	public static final String PHONE_NUMBER_PATTERN_MANDATORY = "^([^0-9*#+]*[0-9*#+]+[^0-9*#+]*)+$"; 
+	public static final String PHONE_NUMBER_PATTERN = "^[0-9/(),.N*;#+ ]+$";
+	public static boolean isPhoneNumber(String phone){
+		boolean isPhoneNumber = phone.matches(PHONE_NUMBER_PATTERN);
+		boolean isValidPhoneNumber = phone.matches(PHONE_NUMBER_PATTERN_MANDATORY);
+		return isPhoneNumber && isValidPhoneNumber;
+	}
+	
+	private final static SimpleDateFormat TIME_STAMP = new SimpleDateFormat("yyyyMMddHHmm");
+	
+	public static String buildRequestCode(String password, long nounce,String message_destination, Date timestamp,String codes) 
+			throws InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException, RequestOverAllowedMaxCharactersException{
+		
+		if(nounce<0){
+			throw new IllegalArgumentException("nounce can't be negative");
+		}
+		if(!isPhoneNumber(message_destination)){
+			throw new IllegalArgumentException("message_destination is not a valid phone number");
+		}
+		if(!codes.matches("^(0|1|\\.)+$")){
+			throw new IllegalArgumentException("code must match ^(0|1|\\.)+$");
+		}
+		
+		StringBuilder request = new StringBuilder("");
+		request.append(nounce);
+		request.append("|");
+		request.append(message_destination);
+		request.append("|");
+		request.append(TIME_STAMP.format(timestamp));
+		request.append(",");
+		request.append(codes);
+		request.append(",");
+		
+		request.append(doHmac(request.toString(), password));
+		
+		if(request.length()>MAXIMUM_CHARACTERS_ALLOWED){
+			throw new RequestOverAllowedMaxCharactersException();
+		}
+		return request.toString();
+	}
+	
+	private static String doHmac(String text, String pass) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException{
+		SecretKeySpec key = new SecretKeySpec(pass.getBytes("UTF-8"), "HmacSHA1");
+		Mac mac = Mac.getInstance("HmacSHA1");
+		mac.init(key);
+		byte[] bytes = mac.doFinal(text.getBytes("UTF-8"));
+		return new String(Base64.encode(bytes));
+	}
+	
+	public static String transformIn256BitHexKey(String key){
+		final int NECESSARY_BYTES = 256/8;
+		byte[] bytes = key.getBytes();
+		List<Integer> result = new LinkedList<Integer>();
+		for(int i=0;i<bytes.length && result.size()<NECESSARY_BYTES;i++){
+		
+			int left = bytes[i] & 0x0f;
+			int right = bytes[i] & 0xf0; right>>=4;
+		
+			if(right!=0){ result.add(right);}
+			result.add(left);
+			
+		}
+		while(result.size()<NECESSARY_BYTES){
+			result.add(0);
+		}
+		
+		List<String> listHexString = Lists.transform(result, new Function<Integer, String>() {
+			@Override
+			public String apply(Integer arg) {
+				return Integer.toHexString(arg);
+			}
+		});
+		return Joiner.on("").join(listHexString);
+	}
+	
+}
